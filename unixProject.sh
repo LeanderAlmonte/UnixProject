039affd2518cd4c6a26f0c14caa04f96a7c28c84
IuliaApi <143552005+IuliaApi@users.noreply.github.com>
2024-11-13 09:45:05 -0500

    Add files via upload

---
+++ unixProject.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+displayingLoadingScreen() {
+echo "Welcome to our Unix Project!"
+echo "Made by Leander Almonte, Brian Arnautov & Iulia Apintilioaie"
+}
+displayingMenu() {
+echo -e "\033[1m1. System Status\033[0m"
+echo -e "\033[1m2. Backup\033[0m"
+echo -e "\033[1m3. Network\033[0m"
+echo -e "\033[1m4. Services\033[0m"
+echo -e "\033[1m5. User Management\033[0m"
+echo -e "\033[1m6. File Management\033[0m"
+echo -e "\033[1m7. Quit\033[0m"
+read -p "Please select a choice from 1-7: " choice
+}
+displayingLoadingScreen
+displayingMenu
+case $choice in
+    1)
+echo "System Status"
+;;
+    2)
+echo "Backup"
+;;
+    3)
+echo "Network"
+;;
+    4)
+echo "Services"
+;;
+    5)
+echo "User Management"
+;;
+    6)
+echo "File Management"
+;;
+    7)
+echo "Thank you for using our computer management program!"
+break
+;;
+    *)
+echo "Invalid option"
+;;
+esac
+}
+}
