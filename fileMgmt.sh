59cee7ab4dade34de7c0fc2544f1080874a46c78
LeanderAlmonte <workleander14@gmail.com>
2024-11-13 10:50:36 -0500

    Created and Finished Search For User Specified File function (not tested)

--- fileMgmt.sh
+++ fileMgmt.sh
@@ -1,12 +1,32 @@
 #!/bin/bash
 
+searchUserSpecifiedFile() {
+echo "Please enter username: "
+read username
+
+if id "$username" &>/dev/null; then
+	echo "Please enter file name: "
+	read file
+	user_home_dir=$(eval echo ~$username)
+	user_path="$user_home_dir/$file"
+		if [ -f "$user_path" ]; then
+			echo "File path found: $user_path"
+		else
+			echo "Error: File $file does not exist or has not been found in $user_home_dir"
+		fi
+fi
+}
+
+
+
+
 echo "File Management"
 
 select option in "Search For User Specified File" "Display 10 largest files" "Display 10 oldest files" "Send email" "Go back";
 do
 	case $option in
 		"Search For User Specified File")
-			echo "Searching"
+			searchUserSpecifiedFile
 			;;
 		"Display 10 largest files")
 			echo "Displaying largest files"
@@ -20,4 +40,5 @@ do
 		"Go back")
 			echo "Going back"
 			;;
+
 
