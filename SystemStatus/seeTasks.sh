32fcfebc1fa903ff39e625ba86bd9af2782c5e82
Brian <143449504+BrianAr17@users.noreply.github.com>
2024-11-13 12:02:10 -0500

    I finished the shell files for the System Status
    It should work with sh for all of them, except for the seeTasks.sh which runs on bash because it uses select. We can modify the code later when we merge it all into one sh file.

---
+++ SystemStatus/seeTasks.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+echo "Choose an operation: "
+
+select operation in "See Running Tasks" "See Task Kill Options" "Kill Task" "Exit"
+
+do
+	case $operation in
+	"See Running Tasks")
+	ps aux
+	;;
+	"See Task Kill Options")
+	kill -l
+	;;
+	"Kill Task")
+	echo "Enter the Kill Option number you want to perform"
+	read killnum
+	echo "Enter the PID of the task that you wish to kill"
+	read PID
+	kill -$killnum $PID
+	;;
+	"Exit")	
+	echo "Exiting..."
+	break	
+	;;
+	*)
+	echo "Invalid choice. Please try again."
+	;;
+	esac
+done
+
+
